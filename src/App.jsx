a1a6bfc64d230276667af6f601e4bea214038341
jetsonf <jetsonf@sfu.ca>
2024-10-29 17:45:58 -0700

    feat: setup leaflet maps

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
 function App() {
   return (
     <>
       Let&apos;s do this
+        <MapContainer style={{height: '100%'}} id='map' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer 
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'          
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+
+        </MapContainer>
     </>
   )
 }
