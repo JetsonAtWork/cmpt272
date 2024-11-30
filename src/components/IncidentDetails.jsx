1120c245775f8e8bc8a7e0834bb69d135fe7ee61
caelwarner <caelawarner@gmail.com>
2024-11-29 20:43:14 -0800

    Changed removed outline style from badges and buttons

--- src/components/IncidentDetails.jsx
+++ src/components/IncidentDetails.jsx
@@ -26,11 +26,11 @@ function IncidentDetailsContent() {
                 <h3 className="text-primary text-lg">Details</h3>
 
                 <div className="flex items-center gap-2">
-                    <button className="btn btn-outline btn-sm">
+                    <button className="btn btn-neutral btn-sm">
                         Update Status
                     </button>
 
-                    <button className="btn btn-outline btn-error btn-sm btn-square">
+                    <button className="btn btn-error btn-sm btn-square">
                         {/*This icon is from Google Material Icons (https://fonts.google.com/icons) (delete)*/}
                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
                     </button>
